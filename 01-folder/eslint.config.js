d52b3823a15a8ab601cafcdccd62d8f84b967244
AmbyHasan <amberhasan237@gmail.com>
2026-06-13 11:51:33 +0530

    first commit

---
+++ 01-folder/eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+  },
+])
+
+//It ensures your code follows best practices for React development and catches potential bugs before runtime. When you save files or run ESLint, it uses this configuration to validate your code.
